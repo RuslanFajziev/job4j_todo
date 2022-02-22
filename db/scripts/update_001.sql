42d832c991993f2aaac289f5a2bfd34b6b1798a7
FaizievRT <rus23f@mail.ru>
2022-02-22 21:40:05 +0300

    0. ToOne v3

---
+++ db/scripts/update_001.sql
@@ -0,0 +1,15 @@
+CREATE TABLE user
+(
+    id       SERIAL PRIMARY KEY,
+    email    VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+);
+
+CREATE TABLE items
+(
+    id          SERIAL PRIMARY KEY,
+    description VARCHAR NOT NULL,
+    created     timestamp without time zone,
+    done        boolean NOT NULL,
+    user_id     INT NOT NULL REFERENCES user (id)
+);
